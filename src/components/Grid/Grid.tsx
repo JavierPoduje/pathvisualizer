e37a88d4c64deeea9875725a5edb771a877b2fdd
JavierPoduje <javier.poduje@gmail.com>
2022-11-19 17:44:19 -0300

    feat: add more cells on the grid

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,19 +1,21 @@
 import Cell from "../Cell/Cell";
 import "./Grid.scss";
 
-const ROWS = 38;
-const COLS = 20;
+const ROWS = 20;
+const COLS = 44;
 
 const Grid = () => {
   return (
-    <section className="grid">
-      {new Array(ROWS)
+    <section className="grid-container">
+      <div className="grid">
+        {new Array(ROWS)
         .fill(0)
         .map((_, row) =>
           new Array(COLS)
-            .fill(0)
-            .map((_, col) => <Cell key={`${row}-${col}`} id={`${row}-${col}`} coord={{ row, col }} />)
+          .fill(0)
+          .map((_, col) => <Cell key={`${row}-${col}`} id={`${row}-${col}`} coord={{ row, col }} />)
         )}
+      </div>
     </section>
   );
 };
